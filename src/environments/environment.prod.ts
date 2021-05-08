9406eaa7058e1d9a05e1bda3d04bf650a053826c
Manju <mskoralli@gmail.com>
2021-05-09 00:33:39 +0530

    Added permissions

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyB4kXka-Z-uiz1X60Vq-T3zOg7H8n6meD0",
+    authDomain: "angular-fs-prod.firebaseapp.com",
+    databaseURL: "https://angular-fs-prod.firebaseio.com",
+    projectId: "angular-fs-prod",
+    storageBucket: "angular-fs-prod.appspot.com",
+    messagingSenderId: "291201278303",
+    appId: "1:291201278303:web:cdddbb855aab593cea16ea"
+
+  }
 };
